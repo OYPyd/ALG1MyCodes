3ea979128d3049b5137969d13b7f8a1d7a77d879
OIPyd <vojtech.vavra@tul.cz>
2024-10-02 09:28:42 +0200

    Alcohol update still temp

--- src/z13alcohol/Alcohol.java
+++ src/z13alcohol/Alcohol.java
@@ -9,10 +9,16 @@ package z13alcohol;
  * @author vojta
  */
 public class Alcohol {
+    /**
+     * volumeOFDrink - dm^3 alias 1/10^3 m^3
+     * alcoholComparedToVolum - 0..1
+     * mass - kg
+     * sex - male: true, female: false
+     */
     public static void Widmark(double volumeOfDrink, double alcoholComparedToVolume, double mass, boolean sex){
-        final float RHO = 0.8f; //
-        final float R; //
-        final float BETA; //
+        final float RHO = 0.8f; //  g/cm^3 .. 10^3 kg/m^3
+        final float R; // 0..1
+        final float BETA; // g/h
         if(sex){
         R = 0.7f;
         BETA = 0.1f;
@@ -21,9 +27,9 @@ public class Alcohol {
         BETA = 0.085f;
         }
         
-        double massEthanol = volumeOfDrink*alcoholComparedToVolume*RHO;
-        double promilesOfAlcohol = massEthanol/mass/R;
-        double speed = mass*BETA;
+        double massEthanol = volumeOfDrink*alcoholComparedToVolume*RHO; // 1000/1000 .. kg
+        double promilesOfAlcohol = massEthanol/mass/R; // 0..1
+        double speed = mass*BETA; // kg * g/h
         
         
     }
